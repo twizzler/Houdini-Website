091aedcc9589cf189d0154ac8000100742508068
Kevin <kbalog124@gmail.com>
2018-11-01 21:57:06 +0100

    Update Application.py

--- Application.py
+++ Application.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy_session import flask_scoped_session
 
-from flask_recaptcha import ReCaptcha
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -14,10 +12,6 @@ from Classes.Database import Database
 app = Flask(__name__)
 
 config = Config(app).build_configuration()
-Config(app).set_flask_config(config)
-
-recaptcha = ReCaptcha(app=app)
-recaptcha.init_app(app)
 
 engine = create_engine("mysql://%s:%s@%s/%s" % (config["database"]["user"],
                                                 config["database"]["pass"],
