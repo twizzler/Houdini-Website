488fc790e75939c75f02faaf1e089011deac6ea9
Kevin <kbalog124@gmail.com>
2018-11-01 21:57:42 +0100

    Update Config.py

--- Classes/Config.py
+++ Classes/Config.py
@@ -26,16 +26,11 @@ class Config:
         }
         self.config['recaptcha'] = {
             'recaptcha_enabled': True,
-            'recaptcha_site_key': '6Lei8BMTAAAAAHJPbumNV5wjZJsY_8axnQvigdVt',
-            'recaptcha_secret_key': '6Lei8BMTAAAAAGpIzsW_zCgqnWA4qEgBm0HArkUf'
+            'recaptcha_site_key': '',
+            'recaptcha_secret_key': ''
         }
         self.config["register"] = {
             "allowed_chars": re.compile(r"^[^<>/{}[\]~`]*$")
         }
 
         return self.config
-
-    def set_flask_config(self, config):
-        self.app.config["RECAPTCHA_ENABLED"] = config["recaptcha"]["recaptcha_enabled"]
-        self.app.config["RECAPTCHA_SITE_KEY"] = config["recaptcha"]["recaptcha_site_key"]
-        self.app.config["RECAPTCHA_SECRET_KEY"] = config["recaptcha"]["recaptcha_secret_key"]
